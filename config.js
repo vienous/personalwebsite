5994c05c07a3e7f0e50b71b198e1658f17659046
vienous <il>
2017-04-19 15:39:14 +0800

    add config file

---
+++ config.js
@@ -0,0 +1,28 @@
+var config = {};
+
+
+config.indexContent = {
+	phone : 18751664739,
+	email : 'kafusky@hotmail.com',
+	school : '江苏师范大学',
+	code :'111111'
+}
+
+config.categories = [
+	'Dapibus ac facilisis in',
+	'Morbi leo risus',
+	'Porta ac consectetur ac',
+	'Vestibulum at eros',
+	'Dapibus ac facilisis in',
+	'Morbi leo risus',
+	'Porta ac consectetur ac',
+	'Vestibulum at eros',
+	];
+
+config.introduce = {
+	large:'Nice responsive template for blogger.',
+	small:'This blogging template use bootstrap and html to create a very nice blogging page with great responsive. '
+};
+
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@ var router = express.Router();
 
 var mongo = require('../DB/mongo');
 var common = require('../common/common.js');
+var config = require('../config.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	mongo.find().then(function(result){
-  		res.render('index',{posts:result});
+	var result = {};
+	result['categories'] = config.categories;
+	result['introduce'] = config.introduce;
+	result['information'] = config.indexContent;
+
+  	mongo.find().then(function(posts){
+  		result['posts'] = posts;
+  		res.render('index',{result:result});
   	});
 });
 module.exports = router;
